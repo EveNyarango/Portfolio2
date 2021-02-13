006f0874ecd96ecc380998b26393f1db00bd336c
EveNyarango <evenyarango@gmail.com>
2021-02-13 15:53:30 +0300

    add form and onclick method

---
+++ index.js
@@ -0,0 +1,4 @@
+function myResponse() {
+    var name = document.getElementById("kerry").value;
+    alert("Thank you " + name + "," + "for your response." + "Looking forward" + " to serve you better.")
+};
